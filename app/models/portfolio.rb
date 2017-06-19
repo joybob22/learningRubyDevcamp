d4813a88ae1e39aa29c21f78c871fdba6983d803
joybob22 <braydenlemke8@gmail.com>
2017-06-19 21:34:00 +0000

    made some scopes

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,11 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+  
+  
+  #these both create a scope that can be used in the controller
+  def self.angular
+    where(subtitle: "Angular")
+  end
+  
+  scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
 end
